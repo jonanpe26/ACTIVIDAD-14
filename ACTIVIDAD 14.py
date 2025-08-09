c5a0e8016ca8a8f1b43d070a9f1d5d77c4f7f643
jonan <heber2025sandoval@gmail.com>
2025-08-08 21:32:32 -0600

    1RO

--- ACTIVIDAD 14.py
+++ ACTIVIDAD 14.py
@@ -1,12 +1,13 @@
 participantes =[]
-while true:
+while True:
     print("1. AGREGAR PARTICIPANTES")
     print("2. MOSTRAR PARTICIPANTES POR NOMBRE")
     print("3. MOSTRAR PARTICIPANTES POR EDAD")
     print("4.  SALIR")
     opcion=input("elija una opcion")
     if opcion =="1":
-        while True:
+        cantidad=int(input("ingrese el numero de participantes"))
+        for _ in range(cantidad):
             dorsal=input("ingrese numero dorsal")
             nombre=input("ingrese nombre completo")
             edad=int(input("ingrese edad"))
@@ -20,9 +21,22 @@ while true:
             })
     elif opcion =="2":
         print("participantes ordenados por nombre")
-        ordenados=
+        lista=participantes[:]
+        for i in range(len(lista)):
+            for j in range(len(lista)):
+                if lista[i]["nombre"]<lista[j]["nombre"]:
+                    lista[i],lista[j]=lista[j],lista[i]
+        for p in lista:
+            print(f"{p["nombre"]}(dorsal{p["dorsal"]},edad{p["edad"]},categoria{p["categoria"]})")
     elif opcion=="3":
         print("participantes ordenados por edad")
+        lista = participantes[:]
+        for i in range(len(lista)):
+            for j in range(i+1, len(lista)):
+                if lista[i]["edad"]>lista[j]["edad"]:
+                    lista[i],lista[j]=lista[j],lista[i]
+        for p in lista:
+            print(f"{p["nombre"]}(dorsal{p["dorsal"]},edad{p["edad"]},categoria{p["categoria"]})")
     elif opcion =="4":
         print("saliendo del programa")
         break
