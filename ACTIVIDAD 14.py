8a3778c033f0db7c280f63bb10848508a6b83f5c
jonan <heber2025sandoval@gmail.com>
2025-08-08 21:13:30 -0600

    1RO

--- ACTIVIDAD 14.py
+++ ACTIVIDAD 14.py
@@ -1,1 +1,32 @@
-S
+participantes =[]
+while true:
+    print("1. AGREGAR PARTICIPANTES")
+    print("2. MOSTRAR PARTICIPANTES POR NOMBRE")
+    print("3. MOSTRAR PARTICIPANTES POR EDAD")
+    print("4.  SALIR")
+    opcion=input("elija una opcion")
+    if opcion =="1":
+        while True:
+            dorsal=input("ingrese numero dorsal")
+            nombre=input("ingrese nombre completo")
+            edad=int(input("ingrese edad"))
+            categoria=input("ingrese categoria")
+
+            participantes.append({
+                "dorsal":dorsal,
+                "nombre":nombre,
+                "edad":edad,
+                "categoria":categoria
+            })
+    elif opcion =="2":
+        print("participantes ordenados por nombre")
+        ordenados=
+    elif opcion=="3":
+        print("participantes ordenados por edad")
+    elif opcion =="4":
+        print("saliendo del programa")
+        break
+
+    else:
+        print("opcion invalida")
+
